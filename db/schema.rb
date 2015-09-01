23e64fa3ed939936642405924b4ea8e8cb3860e9
Bob Zeng <lion123lion2003@126.com>
2015-08-31 19:59:30 -0700

    Fix cookie overflow error by using active record store instead cookie store

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150128032751) do
+ActiveRecord::Schema.define(:version => 20150901025326) do
 
   create_table "groups_users", :id => false, :force => true do |t|
     t.integer "group_id", :null => false
@@ -70,6 +70,16 @@ ActiveRecord::Schema.define(:version => 20150128032751) do
 
   add_index "products", ["family_id"], :name => "index_products_on_family_id"
   add_index "products", ["number"], :name => "index_products_on_number"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "login",                                :default => "",    :null => false
